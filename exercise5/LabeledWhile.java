30d1fd54731dee18cb62e2a2a8db0c8455476b77
Ian Cornett <ian.cornett@outlook.com>
2016-11-05 20:37:40 -0700

    exercise 5 completed

--- exercise5/LabeledWhile.java
+++ exercise5/LabeledWhile.java
@@ -1,3 +1,33 @@
-public class LabeledWhile {
+import java.lang.Throwable;
 
+public class LabeledWhile {
+    static int age;
+
+    public static void main(String[] args) {
+        try {
+            if (args[0] != null) {
+                try {
+                    age = Integer.valueOf(args[0]);
+                } catch (NumberFormatException nfe) {
+                    System.out.println(nfe.getMessage());
+                    age = 0;
+                }
+            }
+        } catch (Exception e) {
+            age = 0;
+        }
+
+        OUTER: while (age <= 21) {
+            if (age == 16) {
+                System.out.println("get your driver\'s license!");
+                age++;
+                continue OUTER;
+            } else if (age == 21) {
+                System.out.println("Congratulations, you\'ve met beer!");
+            } else {
+                System.out.println("Another year...");
+            }
+            age++;
+        }
+    }
 }
